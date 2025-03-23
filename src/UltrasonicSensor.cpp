5234845e8fd11bfb982eeae21a2e3fa594b7432e
Abdullah Ahmed <abdullahahmed2004@gmail.com>
2025-03-23 12:53:46 +0500

    Median and example added

--- src/UltrasonicSensor.cpp
+++ src/UltrasonicSensor.cpp
@@ -6,23 +6,11 @@
 
 #include "UltrasonicSensor.h"
 
-#include <Energia.h>  // For TivaC using Energia
-
 // Constructor with initializer list
 UltrasonicSensor::UltrasonicSensor(int trig, int echo, float temp)
     : trigPin(trig),
       echoPin(echo),
       temperature(temp),
-      timeOut(30000)  // Default timeout (about 5m max distance)
-{
-  // Initialize speed of sound
-  updateSpeedOfSound();
-}
-
-UltrasonicSensor::UltrasonicSensor(int trig, int echo)
-    : trigPin(trig),
-      echoPin(echo),
-      temperature(26),
       timeOut(30000)  // Default timeout (about 5m max distance)
 {
   // Initialize speed of sound
@@ -91,4 +79,44 @@ float UltrasonicSensor::getAverageDistanceCm(int numReadings) {
   }
 
   return total / validReadings;
+}
+
+float UltrasonicSensor::getMedianDistanceCm(int numReadings) {
+  if (numReadings <= 0) {
+    return -1;
+  }
+
+  // Create array to store readings
+  float readings[numReadings];
+  int validReadings = 0;
+
+  // Collect valid readings
+  for (int i = 0; i < numReadings; i++) {
+    float distance = getDistanceCm();
+    if (distance >= 0) {
+      // Insertion sort - insert new reading in the right position
+      int j = validReadings;
+      while (j > 0 && readings[j - 1] > distance) {
+        readings[j] = readings[j - 1];
+        j--;
+      }
+      readings[j] = distance;
+      validReadings++;
+    }
+    // delay(10);  // Short delay between readings
+  }
+
+  if (validReadings == 0) {
+    return -1;
+  }
+
+  // Return median value
+  if (validReadings % 2 == 0) {
+    // Even number of readings, return average of middle two
+    return (readings[validReadings / 2 - 1] + readings[validReadings / 2]) /
+           2.0;
+  } else {
+    // Odd number of readings, return middle one
+    return readings[validReadings / 2];
+  }
 }
